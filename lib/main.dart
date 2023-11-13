79e0e83f28baec106288a8ec8179612c9437f7e4
hiwa Taheri <hiwataheri@hiwas-MBP.fritz.box>
2023-11-13 14:35:18 +0100

    Shimmer Effekt

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,41 +21,52 @@ class MyApp extends StatelessWidget {
 }
 
 class MyImageLoader extends StatelessWidget {
-  const MyImageLoader({Key? key}) : super(key: key);
+  const MyImageLoader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<void>(
-      future: loadImage(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          // Wenn der Ladevorgang abgeschlossen ist, zeige das Bild an
-          return Image.network(
-            'https://www.wikinger-reisen.de/bilder/reiseseiten/nonexistent_image.webp', // Hier verwenden wir eine falsche URL, um einen Fehler zu provozieren
-            width: 300,
-            height: 200,
-            fit: BoxFit.cover,
-          );
-        } else if (snapshot.hasError) {
-          // Wenn ein Fehler auftritt, zeige den Error-Zustand an
-          return Center(
-            child: Text(
-              'Fehler beim Laden des Bildes: ${snapshot.error}',
-              style: TextStyle(color: Colors.red),
-            ),
-          );
-        } else {
-          // Wenn das Bild noch lädt, zeige den Fortschrittsindikator an
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      },
+    return Stack(
+      children: [
+        FutureBuilder<void>(
+          future: loadImage(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Image.network(
+                'https://www.wikinger-reisen.de/bilder/reiseseiten/denali-nationalpark-head4125-t.webp',
+                width: 300,
+                height: 200,
+                fit: BoxFit.cover,
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  'Fehler beim Laden des Bildes: ${snapshot.error}',
+                  style: const TextStyle(color: Colors.red),
+                ),
+              );
+            } else {
+              return Center(
+                child: Shimmer.fromColors(
+                  baseColor: Colors.grey[300]!,
+                  highlightColor: Colors.grey[100]!,
+                  child: Container(
+                    width: 300,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                ),
+              );
+            }
+          },
+        ),
+      ],
     );
   }
 
   Future<void> loadImage() async {
-    // Hier provozieren wir einen Fehler, indem wir versuchen, ein nicht vorhandenes Bild zu laden
-    throw 'Simulierter Fehler: Bild konnte nicht geladen werden';
+    await Future.delayed(const Duration(seconds: 2)); // Künstliche Verzögerung von 2 Sekunden
   }
 }
